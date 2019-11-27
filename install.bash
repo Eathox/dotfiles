4c77e5e26bc36d6a6b60d275b05daf8257761222
pholster <pholster@student.codam.nl>
2019-11-27 15:29:39 +0100

    adding install script

---
+++ install.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+backup () {
+	local TARGET=".zshrc"
+	local BACKUP_LOC="$HOME/$TARGET-backup"
+
+	echo "Backing up original files to '$BACKUP_LOC'"
+	if [[ -e $BACKUP_LOC ]]; then
+		echo "Error: file '$BACKUP_LOC' already exists, aborting"
+		exit
+	fi
+
+	mkdir $BACKUP_LOC
+	mv ~/$TARGET $BACKUP_LOC/$TARGET
+}
+
+link () {
+	local TARGETS=(".zshrc" ".zshrc.functions")
+
+	echo "Linking: ${TARGETS[@]}"
+	for TARGET in "${TARGETS[@]}"; do
+		cp -i $TARGET $HOME
+		rm $TARGET
+		ln "$HOME/$TARGET" $TARGET
+	done
+}
+
+backup
+link
